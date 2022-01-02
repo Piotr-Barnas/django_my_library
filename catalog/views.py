6815a7f1a8e78611b8ded86b6a6591b7db15714d
Piotr-Barnas <piotr.barnas13@gmail.com>
2022-01-03 00:10:17 +0100

    Home page is added

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import Author, Book, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    """View function used for home page"""
+
+    # counts of main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_available': num_available,
+        'num_authors': num_authors,
+    }
+
+    """
+    The render() function accepts the following parameters:
+
+    - the original request object, which is an HttpRequest.
+    - an HTML template with placeholders for the data.
+    - a context variable, which is a Python dictionary, containing the data to insert into the placeholders.
+    """
+
+    return render(request, 'index.html', context=context)
